599801c7f7f727425a061714ab39174ff896e4e6
jairo <jairofilho99@hotmail.com>
2025-05-01 14:55:44 -0300

    atualizando

--- driven.js
+++ driven.js
@@ -1,3 +1,35 @@
-function seleciona(button){
+function selecionaprato(button){
+
+    const parent = button.parentElement;
+
+    const botoes = parent.querySelectorAll("button, div");
+  
+    botoes.forEach(item => item.classList.remove("escolhido"));
+
     button.classList.add("escolhido");
+
+}
+
+function selecionabebida(button){
+
+    const parent = button.parentElement;
+
+    const botoes = parent.querySelectorAll("button, div");
+  
+    botoes.forEach(item => item.classList.remove("escolhido"));
+
+    button.classList.add("escolhido");
+    
+}
+
+function selecionasobremesa(button){
+
+    const parent = button.parentElement;
+
+    const botoes = parent.querySelectorAll("button, div");
+  
+    botoes.forEach(item => item.classList.remove("escolhido"));
+
+    button.classList.add("escolhido");
+    
 }
